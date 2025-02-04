3746fe34707b31cdcc987b4925d6bce67daaf923
Bourichi-Taha <bourichi.taha@gmail.com>
2025-02-04 09:56:25 +0100

    changed port

--- TheExplorer.js
+++ TheExplorer.js
@@ -969,6 +969,6 @@ app.post("/update", async (req, res) => {
   updateWorkersConfig(configUpdates);
 });
 
-app.listen(4010, () => {
-  console.log("server listen to port 4001");
+app.listen(process.env.PORT||8080, () => {
+  console.log("server listen to port "+process.env.PORT||8080);
 });
